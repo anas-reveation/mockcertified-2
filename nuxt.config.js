c5d67de372c480bc1f12904ee1ac424571ea8b5e
shubham chandel <sabfit3@gmail.com>
2022-06-24 19:31:51 +0530

    change: title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mock-certified',
+    title: 'Mock Certified App',
     htmlAttrs: {
       lang: 'en',
     },
