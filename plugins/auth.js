45c8a1e1801ae6643c3becd01c68d051dbae2070
jewells77 <Jewells.joshi@reveation.io>
2022-07-15 15:33:37 +0530

    fix: middleware & plugins

--- plugins/auth.js
+++ plugins/auth.js
@@ -21,13 +21,27 @@ class AuthService {
   }
 }
 
-export default async ({ store }) => {
+export default async ({ app, route, store }) => {
   const authService = new AuthService(store);
   Vue.prototype.$auth = authService;
   Vue.$auth = authService;
   await store.dispatch('auth/load');
-  const isAuthenticated = store.state.auth.isAuthenticated;
-  if (isAuthenticated) {
-    await store.dispatch('testManagement/getUserTests');
-  }
+
+  // Run when app loads
+  await window.onNuxtReady(() => {
+    const isAuthenticated = store.state.auth.isAuthenticated;
+    const homeRoute = route.path === '/' ? true : false;
+    const dashboardRoute = route.path.match(/\/dashboard\/*/g) ? true : false;
+    const categoryRoute = route.path.match(/\/category\/*/g) ? true : false;
+    const testRoute = route.path.match(/\/test\/*/g) ? true : false;
+    const cartRoute = route.path.match(/\/cart\/*/g) ? true : false;
+
+    const loginSignUpRoute = route.path.match(/\/auth\/*/g) ? true : false;
+    if (!isAuthenticated && (dashboardRoute || categoryRoute || testRoute || cartRoute)) {
+      app.router.push('/');
+    }
+    if (isAuthenticated && (homeRoute || loginSignUpRoute)) {
+      app.router.push('/dashboard');
+    }
+  });
 };
--- plugins/deepLink.js
+++ plugins/deepLink.js
@@ -1,12 +1,14 @@
 import { App as CapacitorApp } from '@capacitor/app';
 
-export default async function () {
+export default async function ({ app }, inject) {
   // Deep links
-  CapacitorApp.addListener('appUrlOpen', function (event) {
-    const domain = process.env.DOMAIN;
-    const slug = event.url.split(domain).pop();
-    if (slug) {
-      this.$router.push(slug);
-    }
+  await window.onNuxtReady(() => {
+    CapacitorApp.addListener('appUrlOpen', function (event) {
+      const domain = process.env.DOMAIN;
+      const slug = event.url.split(domain).pop();
+      if (slug) {
+        app.router.push(slug);
+      }
+    });
   });
 }
