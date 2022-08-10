cce3d472b8d8ba8a019e70c98119b29329d872b8
jewells77 <Jewells.joshi@reveation.io>
2022-08-10 14:51:22 +0530

    add: resolver in schema

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const onboardingStripe = /* GraphQL */ `
+  query OnboardingStripe {
+    onboardingStripe
+  }
+`;
 export const getUser = /* GraphQL */ `
   query GetUser($id: ID!) {
     getUser(id: $id) {
