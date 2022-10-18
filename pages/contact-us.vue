db151549830261ad0392f601ee0a99e66a50e618
jewells77 <Jewells.joshi@reveation.io>
2022-10-18 18:31:16 +0530

    fixed: logo

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container px-4">
-    <NavbarLogo />
     <h1 class="mt-5 mb-4 font_size_36">Contact Us</h1>
     <p>
       If you have any questions reach out to us to this email address
