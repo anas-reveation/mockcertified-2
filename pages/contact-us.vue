65d8147e13d0db8ac2d97fd5d6d3d53cda864cb5
jewells77 <Jewells.joshi@reveation.io>
2022-11-02 13:43:52 +0530

    change: contact us msg

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -2,8 +2,10 @@
   <div class="container px-4">
     <h1 class="mt-5 mb-4 font_size_36">Contact Us</h1>
     <p>
-      If you have any questions reach out to us to this email address
-      <span class="fw-bolder">contactus@mockcertified.com.</span>
+      For all your questions and queries please reach us at
+      <a href="mailto:contactus@mockcertified.com" class="fw-bolder text-dark">
+        contactus@mockcertified.com
+      </a>
     </p>
   </div>
 </template>
