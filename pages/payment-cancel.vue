f826d5a5294edb4f1c8dfd9f828758e177e9e36c
jewells77 <Jewells.joshi@reveation.io>
2023-01-30 18:51:43 +0530

    fixed: minor changes

--- pages/payment-cancel.vue
+++ pages/payment-cancel.vue
@@ -3,21 +3,15 @@
     <NavbarLogo />
 
     <img class="position-absolute top-0 end-0 oval_img" src="@/assets/images/oval.jpg" alt="oval" />
-    <h1 class="my-5 text-sm-center font_size_36">Payment Status</h1>
+    <h1 class="my-5 text-center font_size_32">Payment Status</h1>
     <div class="text-center">
       <img src="@/assets/images/failed_icon.svg" alt="failed-icon" />
 
-      <p class="mt-3 font_size_32">
-        Your
-        <br class="d-sm-none" />
-        payment
-        <br class="d-sm-none" />
-        was unsuccessful
-      </p>
+      <p class="mt-3 font_size_24 subtitle_font_size">Your payment was unsuccessful</p>
 
       <div class="mt-5d">
-        <NuxtLink class="btn btn-primary text-white w-75 width_res" to="/category">
-          Go to category
+        <NuxtLink class="btn btn-primary text-white width_res" to="/category">
+          <span class="font_size_18"> Go to category </span>
         </NuxtLink>
       </div>
     </div>
@@ -104,9 +98,7 @@ export default {
 
 <style scoped lang="scss">
 @import '~/assets/css/auth.scss';
-@import '~bootstrap/scss/_functions.scss';
-@import '~bootstrap/scss/_variables.scss';
-@import '~bootstrap/scss/mixins/_breakpoints';
+@import '~/assets/css/bootstrapBreakpoint.scss';
 
 @include media-breakpoint-up(sm) {
   .width_res {
