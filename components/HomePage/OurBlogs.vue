082aae3652cfdf8445ff02f79e58bc00a55ca610
anas <anas.khan@reveation.io>
2023-11-20 11:39:27 +0530

    chnages : add blog scetion

--- components/HomePage/OurBlogs.vue
+++ components/HomePage/OurBlogs.vue
@@ -1,36 +1,34 @@
 <template>
-  <div>
-    <div class="my-5">
-      <h2 class="text-black font-size-44 fw-bolder text-center font_family_poppins_bold my-5">
-        Our Blogs
-      </h2>
-      <div class="container">
-        <div class="row g-4">
-          <div v-for="i in screenWidth > 991 ? 3 : 2" class="col-12 col-md-6 col-lg-4">
-            <div class="card h-100">
-              <img src="@/assets/images/blogs_card.svg" alt="card_1" class="w-100" />
-              <div class="d-flex flex-column justify-content-between h-100 p-3">
-                <div class="">
-                  <p class="fw-bolder font-size-26 mb-0">Nunc sed eldqt lectus Lorem</p>
-                  <p class="font-size-14">1st Aug, 2022</p>
-                  <p class="font-size-14">
-                    Lorem ipsum dolor sit amet, consectetur uadipelioeiusmod tempor incididunt ut
-                    labo ipsum dolor sit amet, consecteturempor uq
-                  </p>
-                </div>
-                <div>
-                  <p class="fw-bolder font-size-16 text-primary text-decoration-underline pt-5">
-                    Read More
-                  </p>
-                </div>
+  <div class="my-5">
+    <h2 class="text-black font-size-44 fw-bolder text-center font_family_poppins_bold my-5">
+      Our Blogs
+    </h2>
+    <div class="container">
+      <div class="row g-4">
+        <div v-for="i in screenWidth > 991 ? 3 : 2" class="col-12 col-md-6 col-lg-4">
+          <div class="card h-100">
+            <img src="@/assets/images/blogs_card.svg" alt="card_1" class="w-100" />
+            <div class="d-flex flex-column justify-content-between h-100 p-3">
+              <div class="">
+                <p class="fw-bolder font-size-26 mb-0">Nunc sed eldqt lectus Lorem</p>
+                <p class="font-size-14">1st Aug, 2022</p>
+                <p class="font-size-14">
+                  Lorem ipsum dolor sit amet, consectetur uadipelioeiusmod tempor incididunt ut labo
+                  ipsum dolor sit amet, consecteturempor uq
+                </p>
+              </div>
+              <div>
+                <p class="fw-bolder font-size-16 text-primary text-decoration-underline pt-5">
+                  Read More
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <p class="fw-bolder font-size-16 text-primary text-decoration-underline text-end my-4">
-          View More
-        </p>
       </div>
+      <p class="fw-bolder font-size-16 text-primary text-decoration-underline text-end my-4">
+        View More
+      </p>
     </div>
   </div>
 </template>
