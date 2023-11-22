647a0fa4d801282fce090e1098f31c55d524b21a
anas <anas.khan@reveation.io>
2023-11-22 17:00:54 +0530

    Changes : home page wallet component changes

--- components/HomePage/YourWallet.vue
+++ components/HomePage/YourWallet.vue
@@ -13,9 +13,9 @@
       </div>
     </div>
     <div class="container">
-      <div v-for="i in 3" class="row align-items-center justify-content-between">
+      <div class="row">
         <div class="col-12 col-lg-5">
-          <div class="card_wallet mb-4 p-3">
+          <div v-for="i in 3" class="card_wallet mb-4 p-3 card_height">
             <p class="font-size-16 fw-bolder font_family_poppins_bold with_underline">
               Lorem ipsum dolor sit amet
             </p>
@@ -25,20 +25,18 @@
             </p>
           </div>
         </div>
-        <div class="col-2 d-none d-lg-flex align-items-center justify-content-center">
-          <img src="~assets/images/ellipse.svg" alt="wallet_button" />
+        <div class="col-2 d-none d-lg-flex align-items-center justify-content-center pb-4">
+          <img src="~assets/images/wallet_element.svg" alt="wallet_button" class="h-100" />
         </div>
-        <div class="col-5 d-none d-lg-block">
-          <p class="font-size-18 fw-bold m-0">
-            MockCertified offers tests at very budget-friendly prices.
-          </p>
-        </div>
-      </div>
-      <div class="row align-items-center justify-content-center text-center d-flex d-lg-none">
-        <div v-for="i in 3" class="col-12 py-3">
-          <p class="font-size-16 font-size-lg-18 fw-bold m-0">
-            MockCertified offers tests at very budget-friendly prices.
-          </p>
+        <div class="col-12 col-lg-5">
+          <div
+            v-for="i in 3"
+            class="card_wallet_2 text-center mb-4 d-flex align-items-center justify-content-center card_height"
+          >
+            <p class="font-size-16 fw-bold m-0 text-primary">
+              MockCertified offers tests at very budget-friendly prices.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -57,6 +55,11 @@
   border: 1px solid rgba(0, 0, 0, 0.08);
   /* Add other card styling properties here */
 }
+.card_wallet_2 {
+  box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.08);
+  border: 1px solid var(--primary);
+  border-radius: 10px;
+}
 
 .with_underline::after {
   content: '';
@@ -65,10 +68,19 @@
   width: 48%; /* Adjust the width of the underline as needed */
   margin-top: 5px; /* Change the color and style as needed */
 }
+.card_height {
+  height: 160px;
+}
 
 @include media-breakpoint-up(md) {
   .w_100 {
     width: 75%;
+  }
+}
+
+@include media-breakpoint-up(xl) {
+  .card_height {
+    height: 145px;
   }
 }
 </style>
