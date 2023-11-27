1b37d7b1cbda06e07f75a08dcb3d3daeeec8d45f
anas <anas.khan@reveation.io>
2023-11-27 15:46:41 +0530

    Chnages  In banner

--- components/MockTestPage/Banner.vue
+++ components/MockTestPage/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg_div">
-    <div class="container">
-      <h2 class="font-size-38 font_family_poppins_bold text-white py-5">Mock Tests</h2>
+    <div class="container py-5">
+      <h2 class="font-size-38 font_family_poppins_bold text-white my-4">Mock Tests</h2>
     </div>
   </div>
 </template>
