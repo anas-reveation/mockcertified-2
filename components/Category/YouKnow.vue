d42b6bf7f0d6313869dc3b406a79dd5b858a8ed8
anas <anas.khan@reveation.io>
2023-11-28 00:14:18 +0530

    Add: you know category

--- components/Category/YouKnow.vue
+++ components/Category/YouKnow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container text-center my-5 py-lg-5 py-0">
+  <div class="container text-center my-4 py-lg-5 py-0">
     <p class="font-size-14 font-size-md-16 font-size-lg-20 fw-bolder">Frequently asked questions</p>
     <h4 class="font-size-24 font-size-md-28 font-size-lg-44 fw-bolder font_family_poppins_bold">
       <span class="text-primary">Everything</span>
@@ -9,7 +9,7 @@
       <div class="col-12 col-lg-5">
         <img src="~/assets/images/you_know_img.svg" alt="you_know_img" class="w-100" />
       </div>
-      <div class="col-12 col-lg-7">
+      <div class="col-12 col-lg-7 mt-4 mt-lg-0">
         <div v-for="i in 5" :key="i" class="btn-group w-100 mb-4 d-flex flex-column">
           <button
             type="button"
@@ -25,11 +25,13 @@
           </button>
           <ul
             v-if="openDropdownIndex === i"
-            class="font-size-10 font-size-md-14 font-size-lg-20 text-start dropdown_menu py-2"
+            class="font-size-10 font-size-md-14 font-size-lg-20 text-start dropdown_menu py-2 px-2"
           >
-            <li><button class="dropdown-item" type="button">Action</button></li>
-            <li><button class="dropdown-item" type="button">Another action</button></li>
-            <li><button class="dropdown-item" type="button">Something else here</button></li>
+            <p class="font-size-12 font-size-lg-14">
+              Lorum dolor sit amet, consectetur uadipelioeiusmpocididunt ut labo ostrud exercitation
+              consequat.ipsum dolamet, secteturempor uq Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            </p>
           </ul>
         </div>
       </div>
