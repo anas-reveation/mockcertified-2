0cf021f5ca72152a6896c94c7ace5394928d5b71
anas <anas.khan@reveation.io>
2023-11-28 11:41:06 +0530

    Add: indivual test page added

---
+++ pages/mocktest/_mocktestId.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ mocktestId }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'homePageLayout',
+
+  async asyncData({ params }) {
+    const mocktestId = params.mocktestId;
+    return { mocktestId };
+  },
+};
+</script>
