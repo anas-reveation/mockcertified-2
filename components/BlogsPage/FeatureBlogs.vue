d67fc87bea4cedecd4aa252fe91fef61a6b68ab8
anas <anas.khan@reveation.io>
2023-12-06 18:41:56 +0530

    Add: blog page

---
+++ components/BlogsPage/FeatureBlogs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my-5 my-5">
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+      <div class="text-center my-4 w_100">
+        <h3 class="font-size-24 font-size-md-28 font-size-lg-44 fw-bolder font_family_poppins_bold">
+          Featured blog posts
+        </h3>
+        <p class="font-size-10 font-size-md-12 font-size-lg-14">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="blog_card"></div>
+        </div>
+        <div class="col-6"></div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/blog.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <BlogsPageBannner />
+    <BlogsPageFeatureBlogs />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'homePageLayout',
+};
+</script>
