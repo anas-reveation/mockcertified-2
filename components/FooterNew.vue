f55491ed62d10cecaeca4c2f94218da8f264522c
anas <anas.khan@reveation.io>
2023-12-06 10:49:48 +0530

    Add: address format

--- components/FooterNew.vue
+++ components/FooterNew.vue
@@ -7,22 +7,30 @@
             <div class="col-12 col-md-6 col-lg-12">
               <div class="">
                 <img src="~assets/images/icon_footer.svg" alt="icon_footer" />
-                <h6 class="font-size-18 pt-4 fw-bolder">
-                  T-8,9 Third floor,shubh complex, Some address –380004
+                <h6 class="font-size-16 font-size-xl-18 pt-4 fw-bolder line_height">
+                  T-8,9 Third Floor, Shubh Complex,<br />
+                  Near Cityshine 2, Shahibaug,<br />
+                  Ahmedabad - 380004, Gujarat, India.
                 </h6>
               </div>
             </div>
             <div class="col-12 col-md-6 col-lg-12">
               <div class="mt-5 mt-md-0 pt-0 mt-lg-5 pt-lg-5">
-                <div class="d-flex align-items-center py-2">
+                <div
+                  class="d-flex align-items-center justify-content-start justify-content-md-end justify-content-lg-start py-2"
+                >
                   <img src="~assets/images/whatsapp.svg" alt="icon_footer" class="contact_icon" />
                   <p class="font-size-16 fw-light m-0 ps-2">India, +91 9898025261</p>
                 </div>
-                <div class="d-flex align-items-center py-2">
+                <div
+                  class="d-flex align-items-center justify-content-start justify-content-md-end justify-content-lg-start py-2"
+                >
                   <img src="~assets/images/whatsapp.svg" alt="icon_footer" class="contact_icon" />
                   <p class="font-size-16 fw-light m-0 ps-2">India, +91 9898025261</p>
                 </div>
-                <div class="d-flex align-items-center py-2">
+                <div
+                  class="d-flex align-items-center justify-content-start justify-content-md-end justify-content-lg-start py-2"
+                >
                   <img src="~assets/images/whatsapp.svg" alt="icon_footer" class="contact_icon" />
                   <p class="font-size-16 fw-light m-0 ps-2">India, +91 9898025261</p>
                 </div>
@@ -33,7 +41,7 @@
         <div class="col-12 col-lg-8 py-3">
           <div class="row g-4 navbar_new font-size-16">
             <div class="col-6 col-md-3">
-              <ul>
+              <ul class="ps-0 ps-lg-auto">
                 <p>About Us</p>
                 <li>lorem</li>
                 <li>lorem</li>
@@ -41,7 +49,7 @@
               </ul>
             </div>
             <div class="col-6 col-md-3">
-              <ul>
+              <ul class="ps-0 ps-lg-auto">
                 <p>Categories</p>
 
                 <li>lorem</li>
@@ -50,7 +58,7 @@
               </ul>
             </div>
             <div class="col-6 col-md-3">
-              <ul>
+              <ul class="ps-0 ps-lg-auto">
                 <p>Learnings</p>
 
                 <li>lorem</li>
@@ -59,7 +67,7 @@
               </ul>
             </div>
             <div class="col-6 col-md-3">
-              <ul>
+              <ul class="ps-0 ps-lg-auto">
                 <p>Option Four</p>
 
                 <li>lorem</li>
@@ -95,6 +103,9 @@
   text-decoration: none;
   list-style-type: none;
   font-size: 14px;
+}
+.line_height {
+  line-height: 1.5rem;
 }
 
 @include media-breakpoint-up(lg) {
