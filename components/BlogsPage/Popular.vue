24274436d0cb9f8d9a4e7565c6be9f920aa77d22
anas <anas.khan@reveation.io>
2023-12-12 12:04:52 +0530

    Add: box shawdow in popular card

--- components/BlogsPage/Popular.vue
+++ components/BlogsPage/Popular.vue
@@ -14,26 +14,28 @@
     </div>
     <div class="container">
       <div class="row gy-4">
-        <div v-for="i in 3" class="col-12 col-md-6 col-lg-4 blog_card_s">
-          <div class="d-flex flex-column">
-            <img src="~assets/images/popular_card.svg" alt="card_blog" class="w-100" />
-            <div class="">
-              <div class="d-flex align-items-center font-size-14 my-3">
-                <div class="">
-                  <p class="bg_green mb-0 p-2">AWS</p>
+        <div v-for="i in 3" class="col-12 col-md-6 col-lg-4">
+          <div class="blog_card_s p-3">
+            <div class="d-flex flex-column">
+              <img src="~assets/images/popular_card.svg" alt="card_blog" class="w-100" />
+              <div class="">
+                <div class="d-flex align-items-center font-size-14 my-3">
+                  <div class="">
+                    <p class="bg_green mb-0 p-2">AWS</p>
+                  </div>
+                  <p class="mb-0 ms-3">5 min read</p>
                 </div>
-                <p class="mb-0 ms-3">5 min read</p>
-              </div>
-              <h4 class="fw-bolder font_family_poppins_bold font-size-24 mb-3">
-                Travelling as a way of self-discovery and progress
-              </h4>
-              <p class="font-size-14 font-size-lg-16 mb-2`">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in
-                eros.
-              </p>
-              <div class="d-flex align-items-center my-3">
-                <p class="font-size-16 mb-0">Read More</p>
-                <img src="~assets/images/blog_arrow.svg" alt="arrow" class="ms-2" />
+                <h4 class="fw-bolder font_family_poppins_bold font-size-24 mb-3">
+                  Travelling as a way of self-discovery and progress
+                </h4>
+                <p class="font-size-14 font-size-lg-16 mb-2`">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim
+                  in eros.
+                </p>
+                <div class="d-flex align-items-center my-3">
+                  <p class="font-size-16 mb-0">Read More</p>
+                  <img src="~assets/images/blog_arrow.svg" alt="arrow" class="ms-2" />
+                </div>
               </div>
             </div>
           </div>
@@ -43,8 +45,11 @@
   </div>
 </template>
 
-<style>
+<style scoped lang="scss">
 .bg_green {
   background-color: rgba(244, 244, 244, 1);
+}
+.blog_card_s {
+  box-shadow: 6px 6px 10px 0 rgba(0, 0, 0, 0.09);
 }
 </style>
